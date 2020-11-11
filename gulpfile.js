297c55894be38b3e57e84e8a8344fed672763c9e
Marinarium <schwmk@gmail.com>
2020-11-11 11:44:42 +0300

    refactor: gulp for js and minimages

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@
 
 const gulp = require('gulp');
 const sass = require('gulp-sass');
-//const concat = require('gulp-concat');
+const concat = require('gulp-concat');
 const sourcemaps = require('gulp-sourcemaps');
 const debug = require('gulp-debug');
 const del = require('del');
@@ -12,8 +12,7 @@ const remember = require('gulp-remember');
 const browserSync = require('browser-sync').create();
 const eslint = require('gulp-eslint');
 const urlAdjuster = require('gulp-css-url-adjuster');
-
-//const isDevelopment = !process.env.NODE_ENV || process.env.NODE_ENV == 'development'  //without sourcemaps use NODE_ENV=production gulp styles
+const imagemin = require('gulp-imagemin');
 
 
 gulp.task('styles', function () {
@@ -27,6 +26,13 @@ gulp.task('styles', function () {
         .pipe(remember('styles'))
         .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest('public/styles'));
+});
+
+gulp.task('scripts', function() {
+    return gulp.src('frontend/js/*.js', {since: gulp.lastRun('scripts')})
+        .pipe(newer('public/js')) //filter does not let no modified files
+        .pipe(concat('main.js'))
+        .pipe(gulp.dest('public/js'));
 });
 
 gulp.task('lint', function () {
@@ -52,13 +58,20 @@ gulp.task('html', function () {
         .pipe(gulp.dest('public'));
 });
 
+gulp.task('images', function(){
+    return gulp.src('frontend/assets/images/**/*.+(png|jpg|gif|svg)')
+        .pipe(imagemin())
+        .pipe(gulp.dest('public/assets/images'))
+});
+
 gulp.task('build', gulp.series(
     'clean',
-    gulp.parallel('styles', 'assets', 'html'))
+    gulp.parallel('styles', 'scripts', 'assets', 'html'))
 );
 
 gulp.task('watch', function () {
     gulp.watch('frontend/styles/**', gulp.series('styles'));
+    gulp.watch('frontend/js/**', gulp.series('scripts'));
     gulp.watch('frontend/assets/**', gulp.series('assets'));
     gulp.watch('frontend/html/**', gulp.series('html'));
 });
@@ -74,4 +87,3 @@ gulp.task('serve', function () {
 gulp.task('dev',
     gulp.series('build', gulp.parallel('watch', 'serve'))
 );
-
