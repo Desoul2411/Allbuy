9c4c711313dfe9e595883eacfb0841e12c549453
Desoul2411 <Desoul2411@gmail.com>
2020-11-16 18:01:58 +0300

    implement submenu

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -6,6 +6,12 @@ let mainMenuList = document.querySelector('.main-menu__list');
 let mainMenuLinks = document.querySelectorAll('.main-menu__link');
 let burgerOpenMenuButton = document.querySelector('.header__open-menu-burger');
 
+let headBottomContent = document.querySelector('.header__bottom-content');
+let submenu = document.querySelector('.main-submenu__sections');
+let mainMenuItems = document.querySelectorAll('.main-menu__item');
+let submenuSections = document.querySelectorAll('.main-submenu__section');
+let submenuCloseButton = document.querySelector('.main-submenu__close-button');
+
 
 //Scroll main menu when click on arrows
 leftMainMenuArrow.addEventListener('click', () => {
@@ -42,6 +48,28 @@ burgerOpenMenuButton.addEventListener('click', () => {
         body.style.overflow = 'hidden';
     }
 });
+
+
+//Show/hide submenu 
+for (let i = 0; i < mainMenuItems.length; i++) {
+    mainMenuItems[i].addEventListener('click', (e) => {
+        headBottomContent.style.height = '100vh';
+        submenu.style.display = 'flex';
+        mainMenuItems.forEach(item => item.classList.remove('active'));
+        submenuSections.forEach(item => item.classList.remove('active'));
+        mainMenuItems[i].classList.add('active');
+        submenuSections[i].classList.add('active');
+    })
+};
+
+//Hide submenu when click on close button 'X'
+submenuCloseButton.addEventListener('click', () => {
+    headBottomContent.style.height = 'auto';
+    submenu.style.display = 'none';
+    mainMenuItems.forEach(item => item.classList.remove('active'));
+    submenuSections.forEach(item => item.classList.remove('active'));
+});
+
 
 
 
