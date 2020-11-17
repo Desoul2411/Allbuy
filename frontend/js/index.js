7a088af1b5d4c6d6949d6679e4d0d6c8b62cbfb4
Desoul2411 <Desoul2411@gmail.com>
2020-11-17 17:49:57 +0300

    implement responsive submenu & search modal layout

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -11,6 +11,35 @@ let submenu = document.querySelector('.main-submenu__sections');
 let mainMenuItems = document.querySelectorAll('.main-menu__item');
 let submenuSections = document.querySelectorAll('.main-submenu__section');
 let submenuCloseButton = document.querySelector('.main-submenu__close-button');
+let submenuColumns = document.querySelectorAll('.main-submenu__column');
+
+
+submenuColumns.forEach((item,index) => {
+    let submenuItemsAmount =  item.querySelectorAll('.main-submenu__column-list-item').length;
+
+    if (submenuItemsAmount > 5){
+        let currentListItems =  item.querySelectorAll('.main-submenu__column-list-item');
+        let showMoreItemsDots = item.querySelector('.main-submenu__show-more-dots');
+
+        showMoreItemsDots.style.display = 'block';
+
+        currentListItems.forEach((listItem,index) => {
+            index > 4 && listItem.classList.add('hidden');
+        });
+
+       showMoreItemsDots.addEventListener('click', () => {
+            currentListItems.forEach((listItem,index) => {
+                if (index > 4) {
+                    listItem.classList.remove('hidden');
+                    showMoreItemsDots.style.display = 'none';
+                };
+            });
+       });
+    } else {
+        item.querySelector('.main-submenu__show-more-dots').style.display = 'none';
+    };
+});
+
 
 
 //Scroll main menu when click on arrows
@@ -38,13 +67,23 @@ mainMenuLinks.forEach( (item) => {
     item.addEventListener('click', (e) => e.preventDefault());
 });
 
+const closeSubmenu = () => {
+    headBottomContent.style.height = 'auto';
+    submenu.style.display = 'none';
+    mainMenuItems.forEach(item => item.classList.remove('active'));
+    submenuSections.forEach(item => item.classList.remove('active'));
+};
+
 //Show/hide mobile menu
 burgerOpenMenuButton.addEventListener('click', () => {
     if (mainMenuContainer.classList.contains('opened')) {
         mainMenuContainer.classList.remove('opened');
+        burgerOpenMenuButton.classList.remove('active');
         body.style.overflow = 'auto';
+        closeSubmenu();
     } else {
         mainMenuContainer.classList.add('opened');
+        burgerOpenMenuButton.classList.add('active');
         body.style.overflow = 'hidden';
     }
 });
@@ -63,12 +102,7 @@ for (let i = 0; i < mainMenuItems.length; i++) {
 };
 
 //Hide submenu when click on close button 'X'
-submenuCloseButton.addEventListener('click', () => {
-    headBottomContent.style.height = 'auto';
-    submenu.style.display = 'none';
-    mainMenuItems.forEach(item => item.classList.remove('active'));
-    submenuSections.forEach(item => item.classList.remove('active'));
-});
+submenuCloseButton.addEventListener('click',closeSubmenu);
 
 
 
